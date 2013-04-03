ffccf9a76bd19fc29bbc896eb3ef2e02f16d6887
Gordon Anderson <gordon.b.anderson@gmail.com>
2013-04-03 16:09:36 +0700

    FIX: Code reformat

--- code/contactpage/ContactPage.php
+++ code/contactpage/ContactPage.php
@@ -7,19 +7,19 @@ class ContactPage extends DemoPage {
 
   function getCMSFields() {
     $fields = parent::getCMSFields();
-    
+
     $gridConfig = GridFieldConfig_RelationEditor::create();
     $gridConfig->getComponentByType( 'GridFieldAddExistingAutocompleter' )->setSearchFields( array( 'PostalAddress' ) );
     $gridConfig->getComponentByType( 'GridFieldPaginator' )->setItemsPerPage( 100 );
-	$gridField = new GridField( "Locations", "List of Addresses:", $this->Locations(), $gridConfig );
-	$fields->addFieldToTab( "Root.Addresses", $gridField );
+    $gridField = new GridField( "Locations", "List of Addresses:", $this->Locations(), $gridConfig );
+    $fields->addFieldToTab( "Root.Addresses", $gridField );
 
     return $fields;
   }
 }
 
 class ContactPage_Controller extends Page_Controller {
-  
+
 
 }
 
